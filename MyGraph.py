dd6d83b26ce53cfa8e7914120793182056b55764
Lucas Walsh <lucas_walsh1@baylor.edu>
2024-08-29 15:13:25 -0500

    initial commit

---
+++ MyGraph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+# Step 1 is to create a virtual environment
+    #PC py -3 -m venv
+    #Mac python3 -m venv
+# Step 2 Activate your VE
+    #pc myvenv/Scripts/activate
+    #mac source myvenv/bin/activate
+# Step 3 Install 3rd party library
+    #pip3 install matplotlib
---
+++ python1.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c   #Debugger stops before executung this line. It will execute through the range. Range is 10, interates 10 times
+
+
+message = "Hello World!"
+
+print(message)
